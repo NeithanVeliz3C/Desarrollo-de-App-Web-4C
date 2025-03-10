a875c061fc03ea798fa7f9671c4395bf85893757
NeithanVeliz3C <isidoraveliz@liceovvh.cl>
2025-03-10 11:15:24 -0300

    ok

---
+++ JS/02_tipos_variables/script.js
@@ -0,0 +1,12 @@
+let numero = 0; //numerico
+let decimal= 2.5 //num float
+let texto = "Hola"; //string
+let booleano = true; //bool
+
+let array = [1, "Hola", 25]; //lista elementos
+let matriz = [[1,2], [3,4]]; //lista dentro de lista
+let objeto = {nombre: "Dany", edad: 18}; //objeto
+
+function sumar(a, b){ //funcion
+    return a + b
+};
---
+++ JS/03_ejercicio_variables/script.js
@@ -0,0 +1,5 @@
+let numero = 3;
+let decimal = 6.7;
+let string = "buenos dias";
+let array = []
+
