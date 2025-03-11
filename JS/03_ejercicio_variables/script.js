f39c40278a233972f50fa42f7087b54ab6e76801
NeithanVeliz3C <isidoraveliz@liceovvh.cl>
2025-03-10 22:36:33 -0300

    ok

--- JS/03_ejercicio_variables/script.js
+++ JS/03_ejercicio_variables/script.js
@@ -1,5 +1,33 @@
-let numero = 3;
-let decimal = 6.7;
-let string = "buenos dias";
-let array = []
+function float(){
+    alert (5.9)
+};
 
+function string(){
+    alert ("Buenos días")
+};
+
+function array(){
+    alert(["Neithan", 17, "hola"])
+}
+
+function bool(){
+    alert (false)
+};
+
+function matriz(){
+    alert ([[3,7] , [2,9]])
+};
+
+function sumar(c, d){
+    let resultado = c + d;
+    alert ("el resultado es:" + resultado);
+    return resultado;
+};
+
+function nulo(){
+    alert ("null")
+};
+
+function indefinido(){
+    alert ("undefined")
+};
