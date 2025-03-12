ab932cb7a20cbb060e798a71d8de606b755b519d
NeithanVeliz3C <isidoraveliz@liceovvh.cl>
2025-03-12 00:31:59 -0300

    ok

--- JS/04_ejercicios/script.js
+++ JS/04_ejercicios/script.js
@@ -22,16 +22,28 @@ let fecha_nac = 1998;
     // Suma 'num1','num2' y 'num3', resta 'num1' de 'num3', multiplica 'num2' por 'num3', y divide 'num1' entre '120'.
     // Muestra los resultados en un mensaje.
     function realizarOperaciones() {
-    
-    alert( );
-    
-    // Tu código aquí
-    }
-    
+        alert ([[num1 + num2 + num3], [num3 - num1], [num2 * num3], [num1 / 120]]);
+    };
+    let num1 = 4;
+    let num2 = 3;
+    let num3 = 5;
+
     // Ejercicio 4: Manipulación de cadenas
     // Instrucciones: Completa la función para construir un mensaje utilizando las variables 'direccion' y 'n_casa'.
     // Luego, muestra la longitud del mensaje y el antepenultimo en el mensaje. (.length)
     function manipularCadenas() {
- 
-    // Tu código aquí
+    let mensaje = "Esta es la dirección: " + direccion + "\nNúmero de casa: " + n_casa;
+    alert(mensaje);
+    alert("Longitud del mensaje: " + mensaje.length);
+
+    if (mensaje.length >= 3) {
+        alert("Antepenúltimo carácter: " + mensaje[mensaje.length - 3]);
+    } else {
+        alert("El mensaje es muy corto.");
     }
+}
+
+let direccion = "doñihue";
+let n_casa = 2030;
+
+manipularCadenas();
