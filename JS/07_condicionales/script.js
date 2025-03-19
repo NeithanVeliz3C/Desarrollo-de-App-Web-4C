c612757227f56d2bead8fc95dbb14c9bb2cb2f3d
NeithanVeliz3C <isidoraveliz@liceovvh.cl>
2025-03-19 16:07:22 -0300

    ok

--- JS/07_condicionales/script.js
+++ JS/07_condicionales/script.js
@@ -1,8 +1,16 @@
+//ejercicio 2
 function clima() {
-    let temperatura = 23;
+    let temperatura = parseInt(prompt("Ingresa la temperatura actual:"));
     if (temperatura <= 0) {
         alert("Hace frío.");
     } else if (temperatura >= 0 && temperatura <=25 ) {
         alert("La temperatura es agradable");
+    } else if (temperatura >=25) {
+        alert("Hace calor");
     }
+}
+
+//ejercicio 3
+function verificacionUsuario() {
+    let nombreUsuario = prompt("Ingresa el nombre de usuario:");
 }
