24fd4c5b601b491c344829fd499a0043aaed6f80
NeithanVeliz3C <isidoraveliz@liceovvh.cl>
2025-03-19 08:44:50 -0300

    ok

---
+++ JS/05_arrays/script.js
@@ -0,0 +1,5 @@
+function Suma_indices() {
+    let array = [1, 2, 3, 4, 5, 6, 7];
+    let suma = array[0] + array[1] + array[2] + array[3] + array[4] + array[5] + array[6];
+    alert(`El arreglo es: ${array.join(" - ")}\nel resultado es: ${suma}`);
+}
--- JS/06_arrays/script.js
+++ JS/06_arrays/script.js
@@ -1,3 +1,5 @@
-function Suma_indices() {
-    
+function Producto_pares() {
+    let arreglo = [2,4,6,7,10,12];
+    let multiplicacion = arreglo[0] * arreglo[1] * arreglo[2] * arreglo[4] * arreglo[5];
+    alert(`El arreglo es: ${arreglo.join(" - ")}\nEl producto es: ${multiplicacion}`);
 }
