87cf87f5d82cf40ee5e122325225bac478cde4a9
NeithanVeliz3C <isidoraveliz@liceovvh.cl>
2025-03-19 22:44:22 -0300

    ok

--- JS/07_condicionales/script.js
+++ JS/07_condicionales/script.js
@@ -13,4 +13,34 @@ function clima() {
 //ejercicio 3
 function verificacionUsuario() {
     let nombreUsuario = prompt("Ingresa el nombre de usuario:");
+    let contraseña = prompt("Ingresa la contraseña:");
+    if (nombreUsuario === "usuario123" && contraseña === "secreto") {
+        alert("Acceso concedido");
+    } else {
+        alert("Acceso denegado");
+    }
+}
+
+//ejercicio 4
+function determinarSigno(){
+    let numero = parseInt(prompt("Ingresa un número:"));
+    if (numero >= 0 ){
+        alert("El número es positivo");
+    } else if (numero <= 0) {
+        alert("El número es negativo");
+    } else if (numero == 0 ){
+        alert("El número e scero");
+    }
+}
+
+//ejercicio 5
+function puntuacionJuego() {
+    let puntuacion = parseInt(prompt("Ingresa la puntuación total del juego:"));
+    if (puntuacion >= 90) {
+        alert("Excelente");
+    } else if (puntuacion <= 89 && puntuacion >=70){
+        alert("Buen trabajo");
+    } else if (puntuacion <= 70 ) {
+        alert("Necesitas mejorar");
+    }
 }
