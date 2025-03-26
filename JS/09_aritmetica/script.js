f3e88ee6615f2421da3ca5545d02e8420d071552
NeithanVeliz3C <165288834+NeithanVeliz3C@users.noreply.github.com>
2025-03-26 08:19:08 -0300

    script.js

--- JS/09_aritmetica/script.js
+++ JS/09_aritmetica/script.js
@@ -22,8 +22,8 @@ function imc() { //ejercicio 15
 
 function promedio() { //ejercicio 16
     let nota1 = prompt("Ingresa tu nota:");
-    let nota2 = prompt("Ingresa tu peso:");
-    let nota3 = prompt("Ingresa tu peso:");
+    let nota2 = prompt("Ingresa tu segunda nota:");
+    let nota3 = prompt("Ingresa tu tercera nota:");
     let promedio = (nota1 + nota2 + nota3) / 3;
     if (promedio <= 4.0) {
         alert("Reprobado");
