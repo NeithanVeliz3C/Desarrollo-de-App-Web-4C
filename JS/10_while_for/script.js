0557bffd38b0e2e9e6bfd0663fdac5a857856f5d
NeithanVeliz3C <isidoraveliz@liceovvh.cl>
2025-03-26 16:10:23 -0300

    ok

--- JS/10_while_for/script.js
+++ JS/10_while_for/script.js
@@ -1,5 +1,5 @@
-function bucleWhile() {
-    let count = 10;
+function bucleWhile() { //ejercicio de prueba 
+    let count = 10; 
     let array = [];
     while(count >= 1) {
         array.push(count);
@@ -8,7 +8,7 @@ function bucleWhile() {
     alert(`Los números son: ${array.join(" - ")}`);
 }
 
-function bucleFor() {
+function bucleFor() { //ejercicio de prueba
     let count = 10;
     let array = [];
     for (let count = 10; count >= 1; count-- ){
@@ -16,3 +16,42 @@ function bucleFor() {
     }
     alert(`Los números son: ${array.join(" - ")}`);
 }
+
+function bucleUno() { //ejercicio 1 
+    let count = 1; 
+    let array = [];
+    while(count <= 30) {
+        array.push(count);
+        count++;
+    }
+    console.log(`Los números son: ${array.join(" - ")}`);
+}
+
+function bucleDos() { //ejercicio 2 100 al 1
+    let count = 100;
+    let array = [];
+    for (let count = 100; count >= 1; count-- ){
+        array.push(count);
+    }
+    alert(`Los números son: ${array.join(" - ")}`);
+}
+
+function contarNumeros() {
+    let n = parseInt(prompt("Ingrese un número entero:"));
+    let count = 1;
+    let array = [];
+    while(count <= n){
+        array.push(count)
+    }
+}
+
+function sumaNumeros() {
+    let suma = 0;
+    let array = [];
+    for(let i = 1; i <= 5; i++){
+        suma += i;
+        array.push(i);
+    }
+    alert(suma)
+}
+
