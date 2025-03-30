afe7beca137788cd4c00b68a3005ab14f6e0b136
NeithanVeliz3C <isidoraveliz@liceovvh.cl>
2025-03-30 17:52:46 -0300

    ok

--- JS/10_while_for/script.js
+++ JS/10_while_for/script.js
@@ -17,16 +17,6 @@ function bucleFor() { //ejercicio de prueba
     alert(`Los números son: ${array.join(" - ")}`);
 }
 
-function bucleUno() { //ejercicio 1 
-    let count = 1; 
-    let array = [];
-    while(count <= 30) {
-        array.push(count);
-        count++;
-    }
-    console.log(`Los números son: ${array.join(" - ")}`);
-}
-
 function bucleDos() { //ejercicio 2 100 al 1
     let count = 100;
     let array = [];
@@ -36,22 +26,43 @@ function bucleDos() { //ejercicio 2 100 al 1
     alert(`Los números son: ${array.join(" - ")}`);
 }
 
-function contarNumeros() {
-    let n = parseInt(prompt("Ingrese un número entero:"));
-    let count = 1;
+function contarNumeros() { //ejercicio 1
+    let count = 1; 
+    let n = parseInt(prompt("Ingresa un número entero:"));
     let array = [];
-    while(count <= n){
-        array.push(count)
+    while(count <= n) {
+        array.push(count);
+        count++;
     }
+    alert(`Los números son: ${array.join(" - ")}`);
 }
 
-function sumaNumeros() {
+function cuentaRegresiva() { //ejercicio 2
+    let count = parseInt(prompt("Ingresa un número entero:"));
+    let array = [];
+    while(count >= 1) {
+        array.push(count);
+        count--;
+    }
+    alert(`La cuenta regresiva es: ${array.join(" - ")}`);
+}
+
+function sumaNumeros() { //ejercicio 3
+    let n = parseInt(prompt("Ingresa un número: "));
     let suma = 0;
     let array = [];
-    for(let i = 1; i <= 5; i++){
-        suma += i; //sumar i en cada iteración 
+    for(let i = 1; i <= n; i++){
+        suma += i; 
         array.push(i);
     }
-    alert(suma)
+    console.log(`La suma de los primeros ${n} números es: ${suma}`);
 }
 
+function multiplosDos() { //ejercicio 4
+    let n = parseInt(prompt("Ingresa un nuúmero límite:"));
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 === 0){
+            console.log(i);
+        }
+    }
+}
