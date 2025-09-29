bc7cfd9dce72105aa95478a39244c44d5065ae81
NeithanVeliz3C <isidoraveliz@liceovvh.cl>
2025-09-29 10:40:57 -0300

    ok

--- JS/12_THIS_JS/this_dojonary/static/js/script.js
+++ JS/12_THIS_JS/this_dojonary/static/js/script.js
@@ -1,34 +1,23 @@
-//botón login
-const loginBtn = document.getElementById('login-btn');
-function cambiarBoton() {
-    if (loginBtn.innerText === "Login") {
-        loginBtn.innerText = "Logout";
+console.log("page loaded...");
+
+function likeBtn(element) {
+    const span = element.querySelector("span");
+    let like = parseInt(span.innerText);
+    like++;
+    span.innerText = like + " likes";
+    alert("¡Te gusta esta publicación!");
+}
+
+
+function login(element) {
+    if (element.innerText == "Login") {
+        element.innerText = "Logout";
     } else {
-        loginBtn.innerText = "Login";
+        element.innerText = "Login";
     }
 }
-loginBtn.addEventListener('click', cambiarBoton);
 
-//botón add definition
-const addDefBtn = document.getElementById('add-def-btn');
-function removerBoton() {
-    addDefBtn.style.display = 'none';
+
+function hide(element) {
+    element.style.display = "none";
 }
-addDefBtn.addEventListener('click', removerBoton);
-
-// botones like
-const likeBtn1 = document.getElementById('like-btn-1');
-const likeBtn2 = document.getElementById('like-btn-2');
-
-function manejarLike(event) {
-    const botonClickeado = event.target;
-    const textoActual = botonClickeado.innerText;
-    let likes = parseInt(textoActual);
-    likes++;
-
-    botonClickeado.innerText = `${likes} likes`;
-    alert('¡Te gusta esta publicación!');
-}
-
-likeBtn1.addEventListener('click', manejarLike);
-likeBtn2.addEventListener('click', manejarLike);
